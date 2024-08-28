a1b673c86b8b2c57ab16268c2e6eba5af11be17d
王雪峰 <w-xuefeng@outlook.com>
2024-08-28 14:01:08 +0800

    feat: add initialStore

--- shared/global-manager/stores.ts
+++ shared/global-manager/stores.ts
@@ -1,3 +1,13 @@
 import Store from 'electron-store';
+import { STORE_KEY } from '../dicts/enums';
+
+const initialStore = {
+  [STORE_KEY.LANG]: 'zh-cn',
+  [STORE_KEY.THEME]: 'auto'
+};
 
 export const GLStore = new Store();
+
+Object.keys(initialStore).forEach((k) =>
+  GLStore.set(k, initialStore[k as keyof typeof initialStore])
+);
