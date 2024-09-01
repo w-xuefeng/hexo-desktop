b7e42d2f7a28fc30da12d997d8d39ba4a05c8cd6
王雪峰 <w-xuefeng@outlook.com>
2024-09-01 13:17:42 +0800

    fix: fixed shell script

--- shared/service-utils/shell.ts
+++ shared/service-utils/shell.ts
@@ -10,15 +10,10 @@ $path | Out-File -FilePath $outfile -Encoding utf8
 
 TARGET_FILE="${envExecutePath}"
 USER_PATH=$PATH
-
-echo "将 PATH 环境变量写入到 $TARGET_FILE"
 echo $USER_PATH >"$TARGET_FILE"
-
 if [ $? -eq 0 ]; then
-  echo "成功将 PATH 环境变量写入到 $TARGET_FILE"
   osascript -e 'tell application "Terminal" to close first window' & exit
 else
-  echo "写入失败，请检查路径是否正确。"
   exit 1
 fi
 `;
